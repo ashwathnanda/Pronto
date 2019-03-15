9397b9ba3631a73fb79a1f74d08d0a220e742a9e
Ashwath Nanda <ashwath5897@gmail.com>
2019-03-15 11:35:02 +0530

    added post requests

--- api_server.py
+++ api_server.py
@@ -11,12 +11,14 @@ from flashtext import KeywordProcessor
 
 app = Flask(__name__)
 api = Api(app)
+parser = reqparse.RequestParser()
 
 key_processor = KeywordProcessor()
 data = pd.read_csv('dataset/data_tag.csv')
 l = list(data['Tag'])
 my_list2 = []
-for i,m in enumerate(l):
+for i in range(len(l)):
+	m = l[i]
 	my_list2.extend(m.split(','))
 	
 #adding the tags onto the processor
@@ -28,11 +30,25 @@ class Fetch_code(Resource):
       ans = key_processor.extract_keywords(code)
       ans.sort()
       ans = ','.join(ans)
-      for i,item in enumerate(l):
-         if(ans == item):
+      for i in range(len(l)):
+         if(ans == l[i]):
             return data['Snippet'][i], 200
-      return 'requirement not found' , 404
+      return 'Requirement not found' , 404
+   
+   def post(self):
+      parser.add_argument('req',type=str)
+      args = parser.parse_args()
+      code = args['req']
+      code.lower()
+      ans = key_processor.extract_keywords(code)
+      ans.sort()
+      ans = ','.join(ans)
+      for i in range(len(l)):
+         if(ans == l[i]):
+            return data['Snippet'][i], 200
+      return 'Requirement not found' , 404
+      
    
 
-api.add_resource(Fetch_code, "/fetch/<string:code>")
+api.add_resource(Fetch_code, "/fetch/<string:code>" , '/')
 app.run(port = 8080)
